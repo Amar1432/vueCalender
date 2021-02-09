2e85a3d5be6133dbe29f0ac7386bed342af5c5c9
Amarjeet Kumar <amarjeet7810@gmail.com>
2021-02-09 17:46:11 +0530

    Chat App improved

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,12 +1,8 @@
-import { computed } from "vue"
-
 export default function(next, store) {
-  const isLoggedIn = computed(() => store.state.isLoggedIn)
-  if (!isLoggedIn.value) {
+  if (!store.state.isLoggedIn) {
     next("/")
     store.commit("setLoginModal", true)
   } else {
-    store.commit("setLoginModal", true)
     next()
   }
 }
--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="text-center mt-6"> 
       <h1 class="text-4xl py-5">This is homepage</h1>
-      <h1 v-if="user">Welcome {{user.email}}</h1>
+      <div v-if="user">
+        <h1>Welcome {{user.email}}</h1>
+      </div>
   </div>
 </template>
 
